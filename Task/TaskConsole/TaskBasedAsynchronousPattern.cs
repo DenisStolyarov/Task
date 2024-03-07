8e21a7c6fd3d4838219ce32d51d2dcdeed85487b
Dzianis Staliarou <Dzianis_Staliarou@epam.com>
2024-03-07 07:55:47 +0400

    Add TAP method example.

---
+++ Task/TaskConsole/TaskBasedAsynchronousPattern.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskConsole;
+
+public static class TaskBasedAsynchronousPattern
+{
+    public static async Task CopyStreamToStreamAsync(Stream source, Stream destination)
+    {
+        var buffer = new byte[0x1000];
+        int numRead;
+
+        while ((numRead = await source.ReadAsync(buffer)) != 0)
+        {
+            await destination.WriteAsync(buffer.AsMemory(0, numRead));
+        }
+    }
+}
